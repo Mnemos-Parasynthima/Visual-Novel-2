fbdc89cbb8260782284162dc7a77c3b51b78c45f
MnemosParasynth <jorgegarcia_tx@outlook.com>
2021-02-14 06:29:29 +0000

    Start screen setup

--- main.js
+++ main.js
@@ -1,28 +1,52 @@
-// Create Visual Novel:
-// HTML element id = getting-started-vn
-// Width = 800
-// Height = 600
-// Images location = img/
-const vn = new VisualNovel( "vn", 800, 600, "img/" );
+/* New visual novel object
+* HTML element id 
+* Width
+* Height
+* Image locations
+*/
+const vn = new VisualNovel(
+    "vn", 
+    800, 
+    600, 
+    "imgs/" 
+);
 
+// Initiates vn
 vn.init();
 
-// Title
-vn.setNovelTitle( "Visual Novel JS", "getting started" );
+// Start screen config (title, bg color, and start button pos)
+vn.setNovelTitle("Visual Novel", "Volume 1");
+vn.setStartScreenBgColor("lightblue");
+vn.setStartScreenMenuPos(500, 500);
 
-// Start screen settings
-vn.setStartScreenBgColor( "black" );
-vn.setStartScreenMenuPos( 550, 500 );
+// Sets vn config (bg color, dialog colo [bg and text])
+vn.setBgColor("lightblue");
+vn.setDialogBgColor("lightgrey");
+vn.setDialogTextColor("black");
 
-// Background
-vn.setBgColor( "black" );
+vn.fadeBg('out', 1000);
+// // text, fade in, duration
+// vn.fadeSceneText( "Mnemos-Parasynthima Presents", "in", 500 );
+// // text, fade out, duration
+// vn.fadeSceneText( "Mnemos-Parasynthima Presents", "out", 500 );
+// // text, fade in, duration
+// vn.fadeSceneText( "Visual Novel: Volume 1", "in", 500 );
+// // text, fade out, duration
+// vn.fadeSceneText( "Visual Novel: Volume 1", "out", 500 );
+vn.fadeBg('in', 1000);
 
-// Dialog settings
-vn.setDialogBgColor( "white" );
-vn.setDialogTextColor( "black" );
+vn.input('name', "What's your name?"); // Asks user for char name
 
-vn.say( "Me", "Hello World" );
-vn.say( "World", "Hi Me" );
+// Creates char object
+const user = {
+  name: {name},
+  nameStyle: "color: darkblue",
+  width: 600, // Temp, testing?
+  height: 600, // Temp, testing?
+  image: "chars/user.jpg", // Future object
+  pos: {
+    x: 1.0,
+    y: 0.007
+  } // Add dialog object (opt)
+}
 
-// Go back to start screen
-vn.reset();
